6f2d7c33b66f6b9b06f03522cf0dde545c704a73
sky-86 <sky86one@gmail.com>
2022-08-02 11:38:34 -0500

    added migrations

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,11 +1,12 @@
 <?php
 
-$servername = "localhost"; //name servername if using xamp or get server name on dashboard if online hosting 
+$servername = "containers-us-west-90.railway.app"; //name servername if using xamp or get server name on dashboard if online hosting 
 $dBUsername = "root";
-$dBPassword = "";
-$dBName = "loginform";
+$dBPassword = "vBhkzPmZNKlmq2EftXY2";
+$dBName = "railway";
+$port = "6169";
 
-$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
+$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName, $port);
 
 if(!$conn){
     die("Connection failed: ".mysqli_connect_error());

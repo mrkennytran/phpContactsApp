084586e18e8d8d42ba5796bec4bf79bbeb915b9c
mrkennytran <97045625+mrkennytran@users.noreply.github.com>
2022-08-02 11:40:15 -0500

    update

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,11 +1,17 @@
 <?php
 
-$servername = "localhost"; //name servername if using xamp or get server name on dashboard if online hosting 
-$dBUsername = "root";
-$dBPassword = "";
-$dBName = "loginform";
 
-$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
+//$servername = "localhost"; //name servername if using xamp or get server name on dashboard if online hosting 
+$servername = "containers-us-west-90.railway.app";
+$dBUsername = "root";
+$dBPassword = "vBhkzPmZNKlmq2EftXY2";
+//$dBName = "loginform";
+$dBName = "railway";
+
+//added
+$port = "6169"; //added
+
+$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName, $port);
 
 if(!$conn){
     die("Connection failed: ".mysqli_connect_error());

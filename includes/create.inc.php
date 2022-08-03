2a193dd97baa85f8759cbb21df95e35651092470
sky-86 <sky86one@gmail.com>
2022-08-03 15:01:20 -0500

    create contacts

--- includes/create.inc.php
+++ includes/create.inc.php
@@ -1,79 +1,117 @@
 <?php 
-if(isset($_POST['signup-submit'])) { //*** 37:00
-    
-    require 'dbh.inc.php';
-
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
-    $nickname = $_POST['nickname'];
-    $company = $_POST['company'];
-    $website = $_POST['website'];
-    $notes = $_POST['notes'];
-    $street = $_POST['street'];
-    $city = $_POST['city'];
-    $state = $_POST['state'];
-    $zipcode = $_POST['zipcode'];
-    $country = $_POST['country'];
-    $emails = $_POST['emails'];
-    $phone_numbers = $_POST['phone_numbers'];
-
-    //Error handler conditions 
-if (empty($username) || empty($email) || empty($password) || empty($passwordRepeat) ) {
-    header("Location: ../signup.php?error=emptyfields&uid=".$username."&mail=".$email);
-    exit();
-} else if (!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9]*$/", $username)) {
-    //If none of the info user enters is correct 
-    header("Location: ../signup.php?error=invalidemail&uid");
-    exit();
-} else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    header("Location: ../signup.php?error=invalidemail&uid=".$username);
-    exit();
-} else if (!preg_match("/^[a-zA-Z0-9]*$/", $username)) { //pregmatic 
-    header("Location: ../signup.php?error=invaliduid&mail=".$email);
-    exit();
-} else if ($password !== $passwordRepeat){
-    //Password entry does not match password repeat 
-    header("Location: ../signup.php?error=passwordcheck&uid=".username."&mail=".$email); //*** */
-    exit();
-} else {
-    //Using a username that already exists/taken by someone else in the database
-    $sql = "SELECT uidUsers FROM users WHERE uidUsers=?"; //using prepared statements - prevent users from corrupting the db 54:00 ? = placeholder for protection
-    $stmt = mysqli_stmt_init($conn);
-
-    if(!mysqli_stmt_prepare($stmt, $sql)){
-        header("Location: ../signup.php?error=sqlerror");
-        exit();
-    } else {
-        mysqli_stmt_bind_param($stmt, "s", $username);
-        mysqli_stmt_execute($stmt); //run stmt inside db
-        mysqli_stmt_store_result($stmt); //return results from db for any matches
-        $resultCheck = mysqli_stmt_num_rows($stmt); //*** */
-        if($resultCheck > 0){
-            header("Location: ../signup.php?error=usertaken&mail=".$email);
-            exit();
-        } else {
-            $sql = "INSERT INTO users (uidUsers, emailUsers, pwdUsers) VALUES (?, ?, ?)";
-            $stmt = mysqli_stmt_init($conn);
-
-            if(!mysqli_stmt_prepare($stmt, $sql)){
-                header("Location: ../signup.php?error=sqlerror");
-                exit();
-            } else {
-                $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
-
-                mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashedPwd); //switched $password to $hashedPwd for user pw protection                 
-                mysqli_stmt_execute($stmt); //run stmt inside db
-                header("Location: ../signup.php?signup=success");
-                exit();            
-            }
-        }
-    }
+function insert_data($sql) {
 
 }
-mysqli_stmt_close($stmt); //closing statement
-mysqli_close($conn); //close connection 
 
+if(isset($_POST['create-submit'])) { //*** 37:00
+  
+  require 'dbh.inc.php';
+
+  $firstname = $_POST['firstname'];
+  $lastname = $_POST['lastname'];
+  $nickname = $_POST['nickname'];
+  $company = $_POST['company'];
+  $website = $_POST['website'];
+  $notes = $_POST['notes'];
+  $street = $_POST['street'];
+  $city = $_POST['city'];
+  $state = $_POST['state'];
+  $zipcode = $_POST['zipcode'];
+  $country = $_POST['country'];
+  $emails = $_POST['emails'];
+  $phone_numbers = $_POST['phone_numbers'];
+
+  $fields = array(
+    $firstname,
+    $lastname,
+    $nickname,
+    $company,
+    $website,
+    $notes,
+    $street,
+    $city,
+    $state,
+    $zipcode,
+    $country,
+    $emails,
+    $phone_numbers,
+  );
+
+  foreach ($fields as &$field) {
+    if (empty($field)) {
+      header("Location: ../create.php?error=emptyfields");
+      exit();
+    }
+  }
+
+  // INSERT ADDRESS
+  $sql = "INSERT INTO addresses 
+  (street,city,state,zip,country) 
+  VALUES (?,?,?,?,?)";
+
+    $stmt = mysqli_stmt_init($conn);
+
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+      header("Location: ../create.php?error=sqlerror");
+      exit();
+    } else {
+      mysqli_stmt_bind_param($stmt, "sssss", 
+        $street,
+        $city,
+        $state,
+        $zipcode,
+        $country
+      );
+
+      mysqli_stmt_execute($stmt); //run stmt inside db
+
+      $address_id = $stmt->insert_id;
+      mysqli_stmt_close($stmt);
+    }
+
+    // INSERT INTO PEOPLE
+  $favorite = "0";
+  $active = "1";
+
+  $sql = "INSERT INTO people 
+  (first_name,last_name,nickname,company,website,notes,favorites,active,address_id) 
+  VALUES (?,?,?,?,?,?,?,?,?)"; //using prepared statements - prevent users from corrupting the db 54:00 ? = placeholder for protection
+
+  $stmt = mysqli_stmt_init($conn);
+
+  if (!mysqli_stmt_prepare($stmt, $sql)) {
+      header("Location: ../create.php?error=sqlerror");
+      exit();
+  } else {
+  mysqli_stmt_bind_param($stmt, "ssssssiii", 
+    $fields[0],
+    $fields[1],
+    $fields[2],
+    $fields[3],
+    $fields[4],
+    $fields[5],
+    $favorite,
+    $active,
+    $address_id
+  );
+
+    mysqli_stmt_execute($stmt); //run stmt inside db
+    $person_id = $stmt->insert_id;
+    mysqli_stmt_close($stmt);
+  }
+
+  // INSERT emails
+  
+
+  // INSERT PHONES
+
+
+  mysqli_stmt_close($stmt); //closing statement
+  mysqli_close($conn); //close connection 
+
+  header("Location: ../create.php?signup=success");
+  exit();
 } else {
-    header("Location: ../signup.php");
+    header("Location: ../create.php");
     exit();     
 }
--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,15 +1,11 @@
 <?php
 
-$server_name = "astamps78515.ipagemysql.com";
-$db_username = "skyler_favors";
-$db_password = "Favors721!";
-$db_name = "skyler_favors";
-
-echo 'here';
+$server_name = "localhost";
+$db_username = "root";
+$db_password = "";
+$db_name = "contacts_db";
 
 $conn = mysqli_connect($server_name, $db_username, $db_password, $db_name);
-
-echo 'after';
 
 if(!$conn){
     die("Connection failed: ".mysqli_connect_error());
